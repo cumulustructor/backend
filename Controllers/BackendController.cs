ffb597bd7028508f99d105e33f39fac2d2e04a1d
Jeffrey Patton <jeffrey@patton-tech.com>
2022-01-31 16:32:08 -0600

    Checking in initial backend api

---
+++ Controllers/BackendController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MCD;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    public class BackendController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [Route("NewDesignDocument")]
+        public MCD.DesignDocument NewDesignDocument(string Cloud)
+        {
+            MCD.DesignDocument DesignDocument = new();
+            switch (Cloud.ToLower())
+            {
+                case "azure":
+                    DesignDocument.Platform.Azure = new()
+                    {
+                        DeploymentLanguage = "",
+                        Tenant = new()
+                        {
+                            displayName = "",
+                            id = "",
+                            domains = null,
+                            Subscriptions = new()
+                            {
+                                new()
+                                {
+                                    displayName = "",
+                                    id = "",
+                                    Resources = new()
+                                }
+                            }
+                        }
+                    };
+                    break;
+            }
+            return DesignDocument;
+        }
+    }
+}
